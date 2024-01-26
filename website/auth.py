9f6a3b90ea8513fd5e546667b74b8e3afbda2f9a
Tanmaycode1 <115889222+Tanmaycode1@users.noreply.github.com>
2024-01-26 12:27:50 +0530

    Intial

---
+++ website/auth.py
@@ -0,0 +1,26 @@
+from . import db
+from flask_login import UserMixin
+
+class User(db.Model,UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150),unique = True)
+    password = db.Column(db.String(150))
+    phone = db.Column(db.String(100))
+    name = db.Column(db.String(150))
+    pin = db.Column(db.String(150))
+    history = db.relationship('transactiondetails')
+
+
+class transactiondetails(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    address = db.Column(db.String(1000))
+    emerphone = db.Column(db.String(100))
+    aadharno = db.Column(db.String(100))
+    bloodgroup = db.Column(db.String(100))
+    gender = db.Column(db.String(100))
+    disease = db.Column(db.String(100))
+    allergies = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
